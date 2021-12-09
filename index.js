79c39944b7c73ea17edfc4dfb7e50dc4ab775fdc
Dan <danielsalazar.cc@gmail.com>
2021-12-08 22:37:42 -0400

    Add scrollback

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const navButton = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav-list')
 const headerArrow = document.querySelector('.header-arrow')
+const scrollButton = document.querySelector('.button-up')
 
 navButton.addEventListener('click', function () {
-  navList.classList.toggle('show')
+  navList.classList.toggle('-show')
   navButton.classList.toggle('open')
-  headerArrow.classList.toggle('-hidden')
+  headerArrow.classList.toggle('-disappear')
   if (navButton.classList.contains('open')) navButton.setAttribute('aria-label', 'Close')
   else navButton.setAttribute('aria-label', 'Open')
+})
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY < 20) {
+    scrollButton.classList.remove('-show')
+    scrollButton.classList.add('-hidden')
+  }
+
+  if (window.scrollY > 100) {
+    scrollButton.classList.contains('-hidden') && scrollButton.classList.remove('-hidden')
+    scrollButton.classList.add('-show')
+  }
+})
+
+scrollButton.addEventListener('click', function () {
+  window.scrollTo(0, 0)
 })
